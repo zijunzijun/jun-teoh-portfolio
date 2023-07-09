b6fded333ab127a836ecbeeffcb8ef00ce91d7ba
zijunzijun <zijun.com@gmail.com>
2023-07-09 15:43:33 +0800

    feat: Add app header and apply initial styling

--- app.vue
+++ app.vue
@@ -1,7 +1,5 @@
 <template>
-  <div>
-    <div class="w-full h-screen bg-cyan-400 grid place-content-center">
-      Install and implement Tailwind CSS
-    </div>
-  </div>
+  <NuxtLayout>
+    <NuxtPage/>
+  </NuxtLayout>
 </template>
---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <TheHeader/>
+  <div class="relative max-w-7xl mx-auto px-4 sm:px-3 md:px-5">
+    <slot/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
