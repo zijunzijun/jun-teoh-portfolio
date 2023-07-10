623b6e6f5edd0e0c5fecb22313c70ad8d5bc19bb
zijunzijun <zijun.com@gmail.com>
2023-07-10 10:33:13 +0800

    feat: Add nuxt-swiper library for carousel functionality

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-fonts'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-fonts', 'nuxt-swiper'],
   googleFonts: {
     families: {
       Ubuntu: [400, 500, 700],
       'Ubuntu Mono': [400, 700]
     }
+  },
+  swiper: {
+    // Swiper options
+    //----------------------
+    // prefix: 'Swiper',
+    // styleLang: 'css',
+    // modules: ['navigation', 'pagination'], // all modules are imported by default
   }
 })
